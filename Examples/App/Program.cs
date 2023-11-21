187a92a30ae83baa30864392204029623b18b0e4
kallebysantos <kalleby_santos@hotmail.com>
2023-11-21 18:03:54 +0000

    📦 NEW: Creating Example project

---
+++ Examples/App/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.SemanticKernel;
+using Together.AI.SemanticKernel;
+
+Console.WriteLine("Hello, World!");
+
+const string API_KEY = "";
+const string MODEL_ID = "Open-Orca/Mistral-7B-OpenOrca";
+
+var kernel = new KernelBuilder().WithTogetherAIService(modelId: MODEL_ID, apiKey: API_KEY).Build();
+
+const string promptTemplate =
+    "<|im_start|> system\nDoes the text contain grammar errors? Answer with (Y/N)\n\n'{{$input}}'\n<|im_end|>\n<|im_start|> assistant\n";
+
+var grammarValidation = kernel.CreateSemanticFunction(promptTemplate);
+
+var result = await grammarValidation.InvokeAsync(
+    input: "I missed the training session this morning",
+    kernel: kernel
+);
+
+Console.WriteLine(result.GetValue<string>());
