13f7b072bdcb794a4abd2e771541b7a99018a5d9
kallebysantos <kalleby_santos@hotmail.com>
2024-03-27 10:18:53 +0000

    📖 DOC: Creating Chat example
    Creating a simple CLI chat example that keeps user history and responds
    based on the given context.

--- Examples/App/TogetherClient.cs
+++ Examples/App/TogetherClient.cs
@@ -42,7 +42,7 @@ public static class TogetherClientExample
         var togetherAI = new TogetherAIClient(client);
 
         // Setup Request Arguments
-        var togetherAIArgs = new TogetherAIRequestArgs()
+        var togetherAIArgs = new TogetherAICompletionArgs()
         {
             Model = modelId,
             MaxTokens = 128,
@@ -85,6 +85,59 @@ public static class TogetherClientExample
         foreach (var token in result?.Data?.First()?.Values ?? [])
         {
             Console.Write(token);
+        }
+    }
+
+    public static async Task RunChatCompletionExample(string apiKey, string modelId)
+    {
+        var client = new HttpClient();
+        client.SetupClient(apiKey);
+
+        var togetherAI = new TogetherAIClient(client);
+
+        var messages = new List<TogetherAIChatMessage>() {
+            new TogetherAIChatSystemMessage("You are a helpful AI assistant.")
+        };
+
+        Console.WriteLine("AI CHAT - write 'exit' to finish.");
+        Console.WriteLine("---------------------------------");
+
+        Console.Write("User: ");
+        string? userInput;
+
+        while (
+            (userInput = Console.ReadLine()) is not null &&
+            !userInput.Contains("exit")
+        )
+        {
+            messages.Add(new TogetherAIChatUserMessage(userInput));
+
+            // Setup Request Arguments
+            var chatArgs = new TogetherAIChatCompletionArgs
+            {
+                Model = modelId,
+                Stop = new string[]{
+                    "</s>",
+                    "[/INST]"
+                },
+                MaxTokens = 512,
+                Messages = messages,
+            };
+
+            // Getting result
+            var result = await togetherAI.GetChatCompletionsAsync(chatArgs);
+
+            if (result?.Choices?.FirstOrDefault()?.Message?.Content is string content)
+            {
+                messages.Add(new TogetherAIChatAssistantMessage(content));
+                Console.WriteLine($"Assistant: {content}");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            Console.Write("User: ");
         }
     }
 }
